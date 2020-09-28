1b55ff526479a3f7b3ca7d7faaa242a7010d888a
edekhayser <erdekhayser@gmail.com>
2020-09-28 17:23:54 -0500

    Created Message interface

--- core/src/edu/rice/comp413fall2020grey/Common/Message.java
+++ core/src/edu/rice/comp413fall2020grey/Common/Message.java
@@ -1,5 +1,12 @@
 package edu.rice.comp413fall2020grey.Common;
 
+import java.util.Date;
+import java.util.UUID;
+
 public interface Message {
+
+  Date getTimestamp();
+
+  UUID getOriginSuperpeer();
 
 }
