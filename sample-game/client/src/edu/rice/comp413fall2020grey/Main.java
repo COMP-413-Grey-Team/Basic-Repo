617fd63ea760e6e4665685ce7a837c2645484f7a
edekhayser <erdekhayser@gmail.com>
2020-09-29 13:31:48 -0500

    Created IntelliJ project for game client and shows prompt for server IP

---
+++ sample-game/client/src/edu/rice/comp413fall2020grey/Main.java
@@ -0,0 +1,10 @@
+package edu.rice.comp413fall2020grey;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showInputDialog("Enter server IP address:", "Game");
+    }
+}
