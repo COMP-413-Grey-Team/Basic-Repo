66bdacc1f8003289a6f70a6ff0d1f278766b0d07
popatr15 <popatr15@github.com>
2020-09-30 16:44:57 -0500

    new branch

--- core/src/edu/rice/comp413fall2020grey/Main.java
+++ core/src/edu/rice/comp413fall2020grey/Main.java
@@ -3,7 +3,8 @@ package edu.rice.comp413fall2020grey;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
+        System.out.println("Hello World");
     }
 
 }
