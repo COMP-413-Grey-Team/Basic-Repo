0ad956df34dac99e43f4bd58a3df966e29c334be
williamsu <ts47@rice.edu>
2020-10-25 13:19:31 -0700

    adding some bson/predicate tests with mongo

--- core/src/edu/rice/comp413fall2020grey/Location/Mongo/Tests/MongoManagerTest.java
+++ core/src/edu/rice/comp413fall2020grey/Location/Mongo/Tests/MongoManagerTest.java
@@ -2,13 +2,22 @@ package edu.rice.comp413fall2020grey.Location.Mongo.Tests;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
 import edu.rice.comp413fall2020grey.Location.Mongo.MongoManager;
+import edu.rice.comp413fall2020grey.Location.interest.CompositePredicate;
+import edu.rice.comp413fall2020grey.Location.interest.EqualityNumberPredicate;
+import edu.rice.comp413fall2020grey.Location.interest.EqualityStringPredicate;
+import edu.rice.comp413fall2020grey.Location.interest.PredicateBiOperator;
+import org.bson.BsonDocument;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+
 import java.util.ArrayList;
 
+import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
 
 /**
@@ -17,29 +26,28 @@ import static com.mongodb.client.model.Filters.eq;
 public class MongoManagerTest {
 
     private static MongoManager mongoManager;
+    private static MongoClient mongoClient;
     private static String password = "UwBNkGQwtdyRBbNf";
+    private final String AIRBNB_DB = "sample_airbnb";
+    private final String ANALYTICS_DB = "sample_analytics";
 
     @BeforeAll
     public static void setupDBConnection() {
-        System.out.println("do here");
         mongoManager = new MongoManager(password);
         mongoManager.connect();
+        mongoClient = mongoManager.getMongoClient();
     }
 
     @Test
     public void testTrialClusterConnection() {
         String database = "sample_airbnb";
-        MongoClient client = mongoManager.getMongoClient();
-        String collectionName = client.getDatabase(database).listCollectionNames().first();
+        String collectionName = mongoClient.getDatabase(AIRBNB_DB).listCollectionNames().first();
         Assertions.assertEquals(collectionName, "listingsAndReviews");
     }
 
     @Test
     public void testTrialClusterConnection2() {
-        String database = "sample_analytics";
-        MongoClient client = mongoManager.getMongoClient();
-
-        ArrayList<String> collectionNames  = client.getDatabase(database)
+        ArrayList<String> collectionNames  = mongoClient.getDatabase(ANALYTICS_DB)
                                                  .listCollectionNames()
                                                  .into(new ArrayList<String>());
 
@@ -50,11 +58,59 @@ public class MongoManagerTest {
 
     @Test
     public void testTrialClusterQuery() {
-        String database = "sample_airbnb";
-        MongoClient client = mongoManager.getMongoClient();
         FindIterable<Document> documents =
-            client.getDatabase(database).getCollection("listingsAndReviews").find(eq("_id", "10009999"));
+            mongoClient.getDatabase(AIRBNB_DB).getCollection("listingsAndReviews").find(eq("_id", "10009999"));
         Assertions.assertNotNull(documents.first());
         System.out.println(documents.first());
     }
+
+    @Test
+    public void testEqualityPredicateQuery() {
+
+        MongoCollection<Document> collection =
+            mongoClient.getDatabase(AIRBNB_DB).getCollection("listingsAndReviews");
+
+        EqualityStringPredicate equalityPred = new EqualityStringPredicate("_id", "10009999");
+        Bson bsonQuery = equalityPred.toMongoQuery(null);
+        FindIterable<Document> expected = collection.find(eq("_id", "10009999"));
+        FindIterable<Document> actual  = collection.find(bsonQuery);
+        Assertions.assertEquals(expected.first(), actual.first());
+        System.out.println(actual.first());
+    }
+
+    @Test
+    public void testCompositePredicateQuery() {
+        MongoCollection<Document> collection =
+            mongoClient.getDatabase(AIRBNB_DB).getCollection("listingsAndReviews");
+
+        CompositePredicate compositePred = new CompositePredicate(
+            new EqualityNumberPredicate("beds", 5, false),
+            new EqualityNumberPredicate("bathrooms", 2, false),
+            PredicateBiOperator.AND);
+
+        Bson bsonQuery = compositePred.toMongoQuery(null);
+        FindIterable<Document> expected = collection.find(and(eq("beds", 5), eq("bathrooms", 2)));
+        FindIterable<Document> actual  = collection.find(bsonQuery);
+
+        Assertions.assertEquals(expected.first(), actual.first());
+        System.out.println(actual.first());
+    }
+
+    @Test
+    public void testEqualityPredicateQueryBack() {
+
+        MongoCollection<Document> collection =
+            mongoClient.getDatabase(AIRBNB_DB).getCollection("listingsAndReviews");
+
+        EqualityStringPredicate equalityPred = new EqualityStringPredicate("_id", "10009999");
+
+        String toJson = equalityPred
+                    .toMongoQuery(null)
+                    .toBsonDocument(null,null) // todo: what is our codecRegistry
+                    .toJson();
+
+        Bson backToBson = BsonDocument.parse(toJson);
+        System.out.println(collection.find(backToBson).first());
+    }
+
 }
