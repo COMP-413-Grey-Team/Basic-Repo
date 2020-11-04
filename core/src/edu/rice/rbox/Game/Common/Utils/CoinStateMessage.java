aa8adbb1ca9752f255a6e0c4218c6af05b16eb62
mja9 <mja9@rice.edu>
2020-11-04 13:08:05 -0600

    Example of proto message  implemented

--- core/src/edu/rice/rbox/Game/Common/Utils/CoinStateMessage.java
+++ core/src/edu/rice/rbox/Game/Common/Utils/CoinStateMessage.java
@@ -1,4 +1,16 @@
 package edu.rice.rbox.Game.Common.Utils;
 
+import edu.rice.rbox.Protos.Generated.GameNetworkProto.CoinState;
+
 public class CoinStateMessage {
+  private CoinState.Builder coinState = CoinState.newBuilder();
+
+  public CoinStateMessage(double x, double y) {
+    coinState.setX(x);
+    coinState.setY(y);
+  }
+
+  public CoinState getCoinState() {
+    return coinState.build();
+  }
 }
