dce5cf291d91c9cb2171c289e7989fc02e2dc749
mja9 <mja9@rice.edu>
2020-11-06 17:19:31 -0600

    Master branch should compile now

--- core/src/edu/rice/rbox/Game/Common/Utils/CoinStateMessage.java
+++ core/src/edu/rice/rbox/Game/Common/Utils/CoinStateMessage.java
@@ -1,16 +1,16 @@
 package edu.rice.rbox.Game.Common.Utils;
 
-//import edu.rice.rbox.Protos.Generated.GameNetworkProto.CoinMessage;
+import network.GameNetworkProto.CoinMessage;
 
 public class CoinStateMessage {
-//  private CoinMessage.Builder coin = CoinMessage.newBuilder();
-//
-//  public CoinStateMessage(double x, double y) {
-//    coin.setX(x);
-//    coin.setY(y);
-//  }
-//
-//  public CoinMessage getCoinMessage() {
-//    return coin.build();
-//  }
+  private CoinMessage.Builder coin = CoinMessage.newBuilder();
+
+  public CoinStateMessage(double x, double y) {
+    coin.setX(x);
+    coin.setY(y);
+  }
+
+  public CoinMessage getCoinMessage() {
+    return coin.build();
+  }
 }
