673b19969164fb26f96e320490d3fa4107930565
mja9 <mja9@rice.edu>
2020-11-06 13:18:26 -0600

    Filling out game service

--- core/src/edu/rice/rbox/Game/Common/Utils/GameGrpc.java
+++ core/src/edu/rice/rbox/Game/Common/Utils/GameGrpc.java
@@ -1,8 +1,33 @@
 package edu.rice.rbox.Game.Common.Utils;
 
-import test.PlayerServerGrpc.PlayerServerImplBase;
+import io.grpc.stub.StreamObserver;
+import network.GameNetworkProto;
+import network.GameServiceGrpc.GameServiceImplBase;
 
-public class GameGrpc extends PlayerServerImplBase {
+public class GameGrpc extends GameServiceImplBase {
 
+
+  @Override
+  public void publishUpdate(GameNetworkProto.UpdateFromClient request,
+                            StreamObserver<GameNetworkProto.UpdateFromServer> responseObserver) {
+    super.publishUpdate(request, responseObserver);
+  }
+
+  @Override
+  public void getAssignedSuperPeer(GameNetworkProto.Empty request,
+                                   StreamObserver<GameNetworkProto.SuperPeerInfo> responseObserver) {
+    super.getAssignedSuperPeer(request, responseObserver);
+  }
+
+  @Override
+  public void initPlayer(GameNetworkProto.InitialPlayerState request,
+                         StreamObserver<GameNetworkProto.UpdateFromServer> responseObserver) {
+    super.initPlayer(request, responseObserver);
+  }
+
+  @Override
+  public void removeMe(GameNetworkProto.PlayerID request, StreamObserver<GameNetworkProto.Empty> responseObserver) {
+    super.removeMe(request, responseObserver);
+  }
 
 }
