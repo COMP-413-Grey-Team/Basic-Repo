908b0b4d8c4933706eb8155304b6b05fb12e225f
Isabella Huang <jh84@rice.edu>
2020-11-07 20:18:32 -0600

    Override GameObjectUUID and ServerUUID equals() and hashCode()

--- core/src/edu/rice/rbox/Common/GameObjectUUID.java
+++ core/src/edu/rice/rbox/Common/GameObjectUUID.java
@@ -1,22 +1,42 @@
 package edu.rice.rbox.Common;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
 public class GameObjectUUID implements Serializable {
 
-  private final UUID uuid;
+    private final UUID uuid;
 
-  private GameObjectUUID(UUID uuid) {
-    super();
-    this.uuid = uuid;
-  }
+    public GameObjectUUID(UUID uuid) {
+        super();
+        this.uuid = uuid;
+    }
 
-  public static GameObjectUUID randomUUID() {
-    return new GameObjectUUID(UUID.randomUUID());
-  }
+    public static GameObjectUUID randomUUID() {
+        return new GameObjectUUID(UUID.randomUUID());
+    }
 
-  public UUID getUUID() {
-    return uuid;
-  }
+    public UUID getUUID() {
+        return uuid;
+    }
+
+    @Override
+    public boolean equals(Object ob) {
+        if (ob == this) {
+            return true;
+        }
+
+        if (ob == null || ob.getClass() != getClass()) {
+            return false;
+        }
+
+        GameObjectUUID gameObjectUUID = (GameObjectUUID) ob;
+        return Objects.equals(gameObjectUUID.getUUID(), uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
 }
--- core/src/edu/rice/rbox/Common/ServerUUID.java
+++ core/src/edu/rice/rbox/Common/ServerUUID.java
@@ -1,22 +1,42 @@
 package edu.rice.rbox.Common;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
 public class ServerUUID implements Serializable {
 
-  private final UUID uuid;
+    private final UUID uuid;
 
-  private ServerUUID(UUID uuid) {
-    super();
-    this.uuid = uuid;
-  }
+    public ServerUUID(UUID uuid) {
+        super();
+        this.uuid = uuid;
+    }
 
-  public static ServerUUID randomUUID() {
-    return new ServerUUID(UUID.randomUUID());
-  }
+    public static ServerUUID randomUUID() {
+        return new ServerUUID(UUID.randomUUID());
+    }
 
-  public UUID getUUID() {
-    return uuid;
-  }
+    public UUID getUUID() {
+        return uuid;
+    }
+
+    @Override
+    public boolean equals(Object ob) {
+        if (ob == this) {
+            return true;
+        }
+
+        if (ob == null || ob.getClass() != getClass()) {
+            return false;
+        }
+
+        ServerUUID serverUUID = (ServerUUID) ob;
+        return Objects.equals(serverUUID.getUUID(), uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
 }
