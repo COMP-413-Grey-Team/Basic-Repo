6328d6d87a6ec0964d3a83d70ffd100e7c4cc1f5
zulokare <lev4@rice.edu>
2020-11-09 16:37:18 -0600

    Comments out files that do not work.

--- core/src/edu/rice/rbox/Game/Common/Utils/PlayerStateMessage.java
+++ core/src/edu/rice/rbox/Game/Common/Utils/PlayerStateMessage.java
@@ -1,21 +1,21 @@
-package edu.rice.rbox.Game.Common.Utils;
-
-import network.GameNetworkProto.PlayerMessage;
-
-public class PlayerStateMessage {
-
-  private final PlayerMessage.Builder state = PlayerMessage.newBuilder();
-
-  public PlayerStateMessage(String color, String name, Integer score, Double x, Double y) {
-    state.setColor(color);
-    state.setName(name);
-    state.setScore(score.toString());
-    state.setX(x);
-    state.setY(y);
-  }
-
-  public PlayerMessage getPlayerMessageMessage() {
-    return this.state.build();
-  }
-
-}
+//package edu.rice.rbox.Game.Common.Utils;
+//
+//import network.GameNetworkProto.PlayerMessage;
+//
+//public class PlayerStateMessage {
+//
+//  private final PlayerMessage.Builder state = PlayerMessage.newBuilder();
+//
+//  public PlayerStateMessage(String color, String name, Integer score, Double x, Double y) {
+//    state.setColor(color);
+//    state.setName(name);
+//    state.setScore(score.toString());
+//    state.setX(x);
+//    state.setY(y);
+//  }
+//
+//  public PlayerMessage getPlayerMessageMessage() {
+//    return this.state.build();
+//  }
+//
+//}
