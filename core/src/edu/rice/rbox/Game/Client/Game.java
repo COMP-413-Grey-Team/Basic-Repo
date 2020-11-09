739b431b3a0676ac97e8a94b194b4c88876db377
zulokare <lev4@rice.edu>
2020-11-09 16:37:18 -0600

    Comments out files that do not work.

--- core/src/edu/rice/rbox/Game/Client/Game.java
+++ core/src/edu/rice/rbox/Game/Client/Game.java
@@ -1,33 +1,33 @@
-package edu.rice.rbox.Game.Client;
-
-import edu.rice.rbox.Game.Common.SyncState.PlayerState;
-
-import java.awt.*;
-import javax.swing.JFrame;
-
-public class Game extends JFrame {
-
-  public Game() {
-
-    initUI();
-  }
-
-  private void initUI() {
-    add(new World(new PlayerState(30, 30, "Evan", Color.BLUE, 0)));
-
-    setResizable(false);
-    pack();
-
-    setTitle("Snake");
-    setLocationRelativeTo(null);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  }
-
-  public static void main(String[] args) {
-
-    EventQueue.invokeLater(() -> {
-      JFrame ex = new Game();
-      ex.setVisible(true);
-    });
-  }
-}
+//package edu.rice.rbox.Game.Client;
+//
+//import edu.rice.rbox.Game.Common.SyncState.PlayerState;
+//
+//import java.awt.*;
+//import javax.swing.JFrame;
+//
+//public class Game extends JFrame {
+//
+//  public Game() {
+//
+//    initUI();
+//  }
+//
+//  private void initUI() {
+//    add(new World(new PlayerState(30, 30, "Evan", Color.BLUE, 0)));
+//
+//    setResizable(false);
+//    pack();
+//
+//    setTitle("Snake");
+//    setLocationRelativeTo(null);
+//    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//  }
+//
+//  public static void main(String[] args) {
+//
+//    EventQueue.invokeLater(() -> {
+//      JFrame ex = new Game();
+//      ex.setVisible(true);
+//    });
+//  }
+//}
