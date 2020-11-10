958d7595e15f3532dbdc30fd717b844cdf1de751
mja9 <mja9@rice.edu>
2020-11-09 23:56:59 -0600

    Failing to push jar

--- core/src/edu/rice/rbox/Networking/ConnectionClient.java
+++ core/src/edu/rice/rbox/Networking/ConnectionClient.java
@@ -1,6 +1,7 @@
 package edu.rice.rbox.Networking;
 
 import io.grpc.*;
+import java.util.concurrent.TimeUnit;
 import network.ConnectionServiceGrpc;
 import network.Network;
 
@@ -12,13 +13,14 @@ public class ConnectionClient {
                                        .usePlaintext(true)
                                        .build();
 
-    // It is up to the client to determine whether to block the call
+    // It is up to the client to de
+    // termine whether to block the call
     // Here we create a blocking stub, but an async stub,
     // or an async stub with Future are always possible.
     ConnectionServiceGrpc.ConnectionServiceBlockingStub stub = ConnectionServiceGrpc.newBlockingStub(channel);
     Network.ServerInformation request =
         Network.ServerInformation.newBuilder()
-            .setServerName("gucci").setServerType("gang")
+            .setServerName("Miguel").setServerType("gang")
             .build();
 
     // Finally, make the call using the stub
