557a673adba400833a30b978735e5de35268f77a
mja9 <mja9@rice.edu>
2020-11-10 02:32:10 -0600

    Implemented all possible messages to be sent from client

--- core/src/edu/rice/rbox/Game/Common/Utils/GameClientGrpc.java
+++ core/src/edu/rice/rbox/Game/Common/Utils/GameClientGrpc.java
@@ -1,5 +1,6 @@
 package edu.rice.rbox.Game.Common.Utils;
 
+import com.google.protobuf.Empty;
 import edu.rice.rbox.Common.GameObjectUUID;
 import edu.rice.rbox.Game.Common.SyncState.PlayerState;
 import io.grpc.ManagedChannel;
@@ -46,6 +47,18 @@ public class GameClientGrpc {
         serverStub.initPlayer(initMessage.getInitialPlayerStateMessage());
 
     return response;
+  }
+
+  public GameNetworkProto.SuperPeerInfo getSuperPeer() {
+    GameNetworkProto.SuperPeerInfo response =
+        serverStub.getAssignedSuperPeer(Empty.newBuilder().build());
+
+    return response;
+  }
+
+  public void remove(GameObjectUUID playerID) {
+    GameNetworkProto.PlayerID.Builder idMessage = GameNetworkProto.PlayerID.newBuilder();
+    serverStub.removeMe(idMessage.setPlayerID(playerID.toString()).build());
   }
 
   public static void main(String args[]) {
--- core/src/edu/rice/rbox/Game/Common/Utils/GameServerGrpc.java
+++ core/src/edu/rice/rbox/Game/Common/Utils/GameServerGrpc.java
@@ -1,4 +1,43 @@
 package edu.rice.rbox.Game.Common.Utils;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class GameServerGrpc {
+
+  public static void main(String args[]) throws IOException, InterruptedException {
+    String ip = "";
+    try(final DatagramSocket socket = new DatagramSocket()){
+      socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+      ip = socket.getLocalAddress().getHostAddress();
+    } catch (SocketException | UnknownHostException e) {
+      System.err.println("Server failed to start.");
+      e.printStackTrace();
+    }
+
+    System.out.println("Server Running on address: " + ip);
+
+
+    // Create a new server to listen on port 8080
+
+    Server server = ServerBuilder.forPort(8080)
+                        .addService(new GameGrpc())
+                        .build();
+
+
+    // Start the server
+    server.start();
+
+    // Server threads are running in the background.
+    System.out.println("Server started");
+
+    // Don't exit the main thread. Wait until server is terminated.
+    server.awaitTermination();
+  }
+
 }
