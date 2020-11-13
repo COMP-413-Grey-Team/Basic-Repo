07f83d52d1327a29592276518a95222413266952
Paul Jiang <siqijiang106@gmail.com>
2020-11-13 17:09:45 -0600

    heartbeatMessage partially implemented

--- core/src/edu/rice/rbox/FaultTolerance/Messages/HeartbeatMessage.java
+++ core/src/edu/rice/rbox/FaultTolerance/Messages/HeartbeatMessage.java
@@ -1,21 +1,42 @@
 package edu.rice.rbox.FaultTolerance.Messages;
 
-import edu.rice.rbox.Common.Message;
-import edu.rice.rbox.Common.ServerUUID;
+import io.grpc.stub.StreamObserver;
+import network.HealthGrpc;
+import network.RBoxProto.HealthCheckRequest;
+import network.RBoxProto.HealthCheckResponse;
 
-import java.util.Date;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
 
 /**
  * Heartbeat message sent from the registrar to the superpeers.
  */
-public class HeartbeatMessage extends Message {
 
-    /**
-     * Constructor for heartbeat messages.
-     * @param timestamp Time this message was sent.
-     * @param originSuperpeer Unique ID for message sender.
-     */
-    protected HeartbeatMessage(Date timestamp, ServerUUID originSuperpeer) {
-        super(timestamp, originSuperpeer, "heartbeat");
+public class HeartbeatMessage extends HealthGrpc.HealthImplBase {
+    private static final Logger logger = Logger.getLogger(HeartbeatMessage.class.getName());
+
+    @Inject
+    public HeartbeatMessage() {
+
+    }
+
+    @Override
+    public void check(HealthCheckRequest request, StreamObserver<HealthCheckResponse> responseObserver) {
+        try {
+//            if (healthCheck.check().get().isHealthy()) {
+                responseObserver.onNext(
+                        HealthCheckResponse.newBuilder().setStatus(HealthCheckResponse.ServingStatus.SERVING).build()
+                );
+//            } else {
+//                responseObserver.onNext(
+//                        HealthCheckResponse.newBuilder().setStatus(HealthCheckResponse.ServingStatus.NOT_SERVING).build()
+//                );
+//            }
+        } catch (Exception ex) {
+            responseObserver.onError(ex);
+        } finally {
+            responseObserver.onCompleted();
+        }
     }
 }
