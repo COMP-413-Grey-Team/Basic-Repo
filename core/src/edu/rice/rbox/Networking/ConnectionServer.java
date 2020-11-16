b8e04dda799c90883589fd70d0ff52edea397185
edekhayser <erdekhayser@gmail.com>
2020-11-16 17:33:57 -0600

    Uncommented files

--- core/src/edu/rice/rbox/Networking/ConnectionServer.java
+++ core/src/edu/rice/rbox/Networking/ConnectionServer.java
@@ -1,41 +1,41 @@
-//package edu.rice.rbox.Networking;
-//
-//import io.grpc.*;
-//import io.grpc.netty.NettyServerBuilder;
-//
-//import java.net.InetAddress;
-//
-//import java.net.DatagramSocket;
-//
-//public class ConnectionServer {
-//  public static void main( String[] args ) throws Exception {
-//
-//
-//    String ip = "";
-//    try(final DatagramSocket socket = new DatagramSocket()){
-//      socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
-//      ip = socket.getLocalAddress().getHostAddress();
-//    }
-//
-//    System.out.println("Server Running on address: " + ip);
-//
-//
-//    // Create a new server to listen on port 8080
-//
-//    Server server = ServerBuilder.forPort(8080)
-//                        .addService(new NetworkImpl())
-//                        .build();
-//
-//
-//    // Start the server
-//    server.start();
-//
-//    // Server threads are running in the background.
-//    System.out.println("Server started");
-//
-//    // Don't exit the main thread. Wait until server is terminated.
-//    server.awaitTermination();
-//  }
-//
-//
-//}
+package edu.rice.rbox.Networking;
+
+import io.grpc.*;
+import io.grpc.netty.NettyServerBuilder;
+
+import java.net.InetAddress;
+
+import java.net.DatagramSocket;
+
+public class ConnectionServer {
+  public static void main( String[] args ) throws Exception {
+
+
+    String ip = "";
+    try(final DatagramSocket socket = new DatagramSocket()){
+      socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+      ip = socket.getLocalAddress().getHostAddress();
+    }
+
+    System.out.println("Server Running on address: " + ip);
+
+
+    // Create a new server to listen on port 8080
+
+    Server server = ServerBuilder.forPort(8080)
+                        .addService(new NetworkImpl())
+                        .build();
+
+
+    // Start the server
+    server.start();
+
+    // Server threads are running in the background.
+    System.out.println("Server started");
+
+    // Don't exit the main thread. Wait until server is terminated.
+    server.awaitTermination();
+  }
+
+
+}
