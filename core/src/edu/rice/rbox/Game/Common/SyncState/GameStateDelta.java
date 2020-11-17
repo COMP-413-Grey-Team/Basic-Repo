eb4fd1f55c0d68f641c7a6056da37dcd77bd448d
unknown <andrew.m.bonar@rice.edu>
2020-11-16 22:22:12 -0600

    functions to translate between game messages and GameState(Delta)

--- core/src/edu/rice/rbox/Game/Common/SyncState/GameStateDelta.java
+++ core/src/edu/rice/rbox/Game/Common/SyncState/GameStateDelta.java
@@ -1,25 +1,20 @@
 package edu.rice.rbox.Game.Common.SyncState;
 
 import edu.rice.rbox.Common.GameObjectUUID;
+import network.GameNetworkProto;
 
 import java.util.HashSet;
 
 // Sent from client to server
 public class GameStateDelta {
 
-  public enum MovingRooms {
-    NOT,
-    LEFT,
-    RIGHT;
-  }
-
   public GameObjectUUID playerUUID;
   public PlayerState updatedPlayerState;
-  public MovingRooms movingRooms;
+  public GameNetworkProto.UpdateFromClient.MovingRooms movingRooms;
 
   public HashSet<GameObjectUUID> deletedCoins;
 
-  public GameStateDelta(GameObjectUUID playerUUID, PlayerState updatedPlayerState, HashSet<GameObjectUUID> deletedCoins, MovingRooms movingRooms) {
+  public GameStateDelta(GameObjectUUID playerUUID, PlayerState updatedPlayerState, HashSet<GameObjectUUID> deletedCoins, GameNetworkProto.UpdateFromClient.MovingRooms movingRooms) {
     this.playerUUID = playerUUID;
     this.updatedPlayerState = updatedPlayerState;
     this.deletedCoins = deletedCoins;
