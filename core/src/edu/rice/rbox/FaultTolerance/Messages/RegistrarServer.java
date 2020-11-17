db30841cde7601d88622b01f4d6ffc3b6eb58afd
Paul Jiang <siqijiang106@gmail.com>
2020-11-16 18:02:18 -0600

    Election class partially implemented RegistrarServer initialized

---
+++ core/src/edu/rice/rbox/FaultTolerance/Messages/RegistrarServer.java
@@ -0,0 +1,91 @@
+package edu.rice.rbox.FaultTolerance.Messages;
+
+import com.google.protobuf.Timestamp;
+import io.grpc.BindableService;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
+import java.util.UUID;
+import network.RBoxProto;
+import network.RegistrarGrpc;
+
+public class RegistrarServer {
+
+        private Server server;
+
+        private UUID serverUUID;
+
+        /**
+         * Constructor for the registrar's Grpc server
+         * @param services this is a list of all the implemented services that this server will use (defines RPCs that
+         *                 can be called on it)
+         * @param serverUUID this is the UUID of the superpeer that this server will be running on
+         */
+        public RegistrarServer (List<BindableService> services, UUID serverUUID) {
+                var temp =  ServerBuilder.forPort(8080);
+                services.forEach((s) -> temp.addService(s));
+                this.server = temp.build();
+                this.serverUUID = serverUUID;
+        }
+
+        /**
+         * This method starts up the system from the superpeer's perspective. It does the following:
+         * Starts up its own gRPC server
+         * Creates a gRPC client for registrar and returns it
+         * Sends the registrar a connectTo message, so the registrar now has a stub to this superpeer's server
+         *
+         * @param registrarIP the IP and port number that the registrar's server is running on (in the string form
+         *                    "ip:port")
+         * @return a stub to the registrar
+         */
+        public RegistrarGrpc.RegistrarBlockingStub startUp(String registrarIP) throws Exception {
+                // start up the server
+                this.server.start();
+
+                // make channel to the registrar using its IP and port num
+                ManagedChannel channel = ManagedChannelBuilder.forTarget(registrarIP)
+                        .usePlaintext(true)
+                        .build();
+
+                // create the grpc client
+                RegistrarGrpc.RegistrarBlockingStub stub2registrar = RegistrarGrpc.newBlockingStub(channel);
+
+                // Find public IP address - UNTESTED
+                String systemipaddress = "";
+                URL url_name = new URL("http://bot.whatismyipaddress.com");
+                BufferedReader sc =
+                        new BufferedReader(new InputStreamReader(url_name.openStream()));
+                systemipaddress = sc.readLine().trim();
+
+
+                // send the registrar a Connect message - Need the other files!
+                long millis = System.currentTimeMillis();
+                Timestamp timestamp = Timestamp.newBuilder().setSeconds(millis / 1000)
+                        .setNanos((int) ((millis % 1000) * 1000000)).build();
+
+                RBoxProto.BasicInfo info = RBoxProto.BasicInfo.newBuilder()
+                        .setSenderUUID(this.serverUUID.toString())
+                        .setTime(timestamp)
+                        .build();
+
+                RBoxProto.ConnectMessage request =
+                        RBoxProto.ConnectMessage.newBuilder()
+                                .setConnectionIP(systemipaddress + ":8080")
+                                .setSender(info)
+                                .build();
+
+
+                stub2registrar.connect(request);
+
+                return stub2registrar;
+        }
+
+
+
+
+}
