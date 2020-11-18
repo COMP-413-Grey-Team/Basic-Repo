de32971c9f61e12028b0e0d9fbc8156c9cd43cdc
Alice Wong <ajw7@rice.edu>
2020-11-18 17:16:26 -0600

    create file for replication unit tests

---
+++ core/test/edu/rice/rbox/Replication/Test.java
@@ -0,0 +1,28 @@
+package edu.rice.rbox.Replication;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit tests for ReplicaManager.
+ */
+public class Test extends TestCase {
+
+  public void setUp() throws Exception {
+    // TODO
+  }
+
+  /**
+   * Send message to fake server, and verify behaviors from the server side.
+   */
+  public void testSubscribeMsg() {
+    // TODO
+  }
+
+  public void testUnsubscribeMsg() {
+    // TODO
+  }
+
+  public void testUpdate() {
+    // TODO
+  }
+}
