2576ea391f23d1695376013f6bea5116dc7a21d9
Tim Cheng <timcheng@Tims-MacBook-Air.local>
2020-11-18 15:37:12 -0600

    uncommented out the class

--- core/src/edu/rice/rbox/Location/locator/Tests/LocatorMainImplTests.java
+++ core/src/edu/rice/rbox/Location/locator/Tests/LocatorMainImplTests.java
@@ -1,42 +1,41 @@
-//package edu.rice.rbox.Location.locator.Tests;
-//
-//import com.mongodb.client.model.Filters;
-//import edu.rice.rbox.Location.locator.LocatorMainImpl;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.UUID;
-//
-//public class LocatorMainImplTests {
-//
-//
+package edu.rice.rbox.Location.locator.Tests;
+
+import edu.rice.rbox.Location.locator.LocatorMainImpl;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class LocatorMainImplTests {
+
+
 //    private static LocatorMainImpl locatorMain = new LocatorMainImpl();
-//    private final UUID UUID_ONE = UUID.fromString("48804d18-22e5-11eb-adc1-0242ac120002");
-//    private final UUID UUID_TWO = UUID.randomUUID();
-//
-//    @Test
-//    public void testUpdateOne() {
-//
-//        // Create some random object to store in the document
-//        HashMap<String, Object> map = new HashMap<>();
-//        ArrayList object1 = new ArrayList();
-//        object1.add("abc");
-//        Integer object2 = 11;
-//        map.put("one", object1);
-//        map.put("two", object2);
-//
-//        locatorMain.updatePrimary(UUID_ONE, map);
+    private final UUID UUID_ONE = UUID.fromString("48804d18-22e5-11eb-adc1-0242ac120002");
+    private final UUID UUID_TWO = UUID.randomUUID();
+
+    @Test
+    public void testUpdateOne() {
+
+        // Create some random object to store in the document
+        HashMap<String, Object> map = new HashMap<>();
+        ArrayList object1 = new ArrayList();
+        object1.add("abc");
+        Integer object2 = 11;
+        map.put("one", object1);
+        map.put("two", object2);
+
+//        locatorMain.(UUID_ONE, map);
 //        locatorMain.queryInterest(UUID_ONE, Filters.gte("two", 11));
-//    }
-//
-//    @Test
-//    public void testDelete() {
+    }
+
+    @Test
+    public void testDelete() {
 //        locatorMain.removePrimary(UUID_ONE);
-//    }
-//
-//
-//
-//
-//
-//}
+    }
+
+
+
+
+
+}
