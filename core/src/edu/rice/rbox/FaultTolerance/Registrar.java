0c0bce12c68b1c1945446ab79ed39bd1c3342a71
popatr15 <popatr15@github.com>
2020-11-28 19:29:38 -0600

    Added mongo start up to registrar

--- core/src/edu/rice/rbox/FaultTolerance/Registrar.java
+++ core/src/edu/rice/rbox/FaultTolerance/Registrar.java
@@ -1,6 +1,11 @@
 package edu.rice.rbox.FaultTolerance;
 
 import com.google.protobuf.Empty;
+import com.mongodb.Mongo;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import edu.rice.rbox.Location.Mongo.MongoManager;
 import edu.rice.rbox.Networking.NetworkImpl;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
@@ -18,6 +23,7 @@ import network.RegistrarGrpc;
 public class Registrar {
 
     private ConnectionManager connManager;
+    private String ipAddress = "";
 
     private RegistrarGrpc.RegistrarImplBase superPeerServiceImpl = new RegistrarGrpc.RegistrarImplBase() {
 
@@ -43,7 +49,9 @@ public class Registrar {
             String senderUUID = request.getSender().getSenderUUID();
             String senderHostnameInfo = request.getConnectionIP();
 
-            Registrar.this.connManager.addSuperPeer(senderHostnameInfo, UUID.fromString(senderUUID));
+            RegistrarGrpc.RegistrarBlockingStub spStub = Registrar.this.connManager
+                                                            .addSuperPeer(senderHostnameInfo, UUID.fromString(senderUUID));
+            spStub.connect(RBoxProto.ConnectMessage.newBuilder().setConnectionIP(ipAddress).build());
             com.google.protobuf.Empty empty = com.google.protobuf.Empty.newBuilder().build();
             responseObserver.onNext(empty);
             responseObserver.onCompleted();
@@ -76,10 +84,16 @@ public class Registrar {
 
 
     public Registrar() {
-        // TODO: Setup Mongo, have a clean database and clean connection, make sure to clear them when starting a new system
-
-        // TODO: set up the connection manager / Mongo stuff
-        this.connManager = new ConnectionManager(null, null);
+        MongoManager mongoMan = new MongoManager();
+        mongoMan.connect();
+        MongoClient client = mongoMan.getMongoClient();
+        client.getDatabase(MongoManager.DB_NAME).drop();
+        MongoDatabase db = client.getDatabase(MongoManager.DB_NAME);
+        db.createCollection(MongoManager.CLIENT_COLLECTION);
+        db.createCollection(MongoManager.SUPERPEER_COLLECTION);
+        db.createCollection(MongoManager.COLLECTION_NAME);
+        this.connManager = new ConnectionManager(db.getCollection(MongoManager.SUPERPEER_COLLECTION),
+            db.getCollection(MongoManager.CLIENT_COLLECTION));
     }
 
     public void init() throws  Exception {
@@ -87,12 +101,11 @@ public class Registrar {
         try(final DatagramSocket socket = new DatagramSocket()){
             socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
             ip = socket.getLocalAddress().getHostAddress();
+            ipAddress = ip;
         }
 
         System.out.println("Server Running on address: " + ip);
 
-        // TODO: Create Connection Manager
-        // TODO: Start grpc server with proper services
 
         // Create a new server to listen on port 8080
 
@@ -119,7 +132,8 @@ public class Registrar {
 
 
     public static void main( String[] args ) throws Exception {
-
+        Registrar reg = new Registrar();
+        reg.init();
 
 
     }
