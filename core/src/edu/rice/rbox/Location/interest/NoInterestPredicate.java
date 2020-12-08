99854ed7e18b9f749e938546e532e21ecfc23748
Alice Wong <ajw7@rice.edu>
2020-12-08 14:08:10 -0600

    fix errors so registrar compiles

--- core/src/edu/rice/rbox/Location/interest/NoInterestPredicate.java
+++ core/src/edu/rice/rbox/Location/interest/NoInterestPredicate.java
@@ -1,6 +1,8 @@
 package edu.rice.rbox.Location.interest;
 
 import com.mongodb.client.model.Filters;
+import edu.rice.rbox.Common.GameObjectUUID;
+import edu.rice.rbox.ObjStorage.ObjectLocationStorageInterface;
 import org.bson.conversions.Bson;
 
 import java.io.Serializable;
@@ -11,7 +13,7 @@ public class NoInterestPredicate implements InterestPredicate {
     public NoInterestPredicate() {}
 
     @Override
-    public Bson toMongoQuery(HashMap<String, Serializable> map) {
+    public Bson toMongoQuery(GameObjectUUID relative_object_uuid, ObjectLocationStorageInterface adapter) {
         return Filters.exists("field_that_should_never_be_used");
     }
 }
