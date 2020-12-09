4840c2c18a7146c4d97be8fb0098c773ad480a6a
Alice Wong <ajw7@rice.edu>
2020-12-08 18:06:16 -0600

    implement assignGameRooms in replica manager and initialize rooms in superpeer

--- core/src/edu/rice/rbox/Replication/ReplicaManagerGrpc.java
+++ core/src/edu/rice/rbox/Replication/ReplicaManagerGrpc.java
@@ -39,6 +39,8 @@ public class ReplicaManagerGrpc implements ObjectLocationReplicationInterface {
     private HashMap<GameObjectUUID, Integer> timeout = new HashMap<>();                   // Primary => timeout
     private final int initial_value = 50;
 
+    private List<Integer> assignedRooms;    // rooms assigned to this superpeer by the registrar
+
     private HashMap<ServerUUID, RBoxServiceGrpc.RBoxServiceBlockingStub> blockingStubs = new HashMap<>();
     private HashMap<ServerUUID, RBoxServiceGrpc.RBoxServiceStub> stubs = new HashMap<>();
     private RegistrarGrpc.RegistrarBlockingStub registrarBlockingStub;
@@ -141,7 +143,6 @@ public class ReplicaManagerGrpc implements ObjectLocationReplicationInterface {
 
         @Override
         public void connect(RBoxProto.ConnectMessage request, StreamObserver<Empty> responseObserver) {
-            System.out.println(request.getSender().getSenderUUID());
             ServerUUID superpeerUUID = new ServerUUID(UUID.fromString(request.getSender().getSenderUUID()));
             String superpeerIP = request.getConnectionIP();
 
@@ -165,7 +166,9 @@ public class ReplicaManagerGrpc implements ObjectLocationReplicationInterface {
         @Override
         public void assignGameRooms(network.RBoxProto.GameRooms request,
                                     io.grpc.stub.StreamObserver<com.google.protobuf.Empty> responseObserver) {
-            // TODO: IMPLEMENT THIS PEOPLES!!!
+            // Save the list of assigned rooms locally so we can initialize the rooms from the superpeer
+            assignedRooms = request.getAssignedRoomsList();
+            responseObserver.onCompleted();
         }
     };
 
@@ -395,5 +398,8 @@ public class ReplicaManagerGrpc implements ObjectLocationReplicationInterface {
             }
         });
     }
+
+    /* Getter for initializing game rooms associated with this superpeer. */
+    public List<Integer> getAssignedRooms() { return assignedRooms; }
 
 }
